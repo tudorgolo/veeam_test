34f7ef4432836bd3010661e2c5012e2a80037878
tudor.gologan <tudor.gologan@live.com>
2024-05-23 20:50:27 +0300

    first commit

---
+++ Monitor/Monitor.cs
@@ -0,0 +1,22 @@
+﻿namespace App {
+    public class Monitor
+    {
+        static void Main() {
+            var processChecker = new ProcessChecker();
+
+            Timer timer = new(processChecker.CheckProcess, processChecker, 0, 10000);
+        }
+    }
+
+    class ProcessChecker {
+        public void CheckProcess(Object? autoEvent) {
+            // var names = new[] { "Ana", "Felipe", "Emillia" };
+            // foreach(var name in names) 
+            // {
+            //     Console.WriteLine($"Hello {name}");
+            // }
+            Console.WriteLine("Hello World");
+
+        }
+    }
+}
